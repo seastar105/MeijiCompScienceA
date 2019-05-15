a55313e5b1ab628282d2fca873547511f6b2feb2
seastar105 <seastar105@gmail.com>
2019-05-15 15:18:30 +0900

    After Handout

---
+++ compscienceAmm_cs_exe/JEONHAESUNG_Week2/Mandatory2/ass2.c
@@ -0,0 +1,92 @@
+#include<gd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define BUF_MAX 256
+
+int main(const int argc, const char *argv[]) {
+    FILE *out, *in, *fin;
+    gdImagePtr im,im_pad,im_new;
+    int width, height, i, j, k, l, pixel, color,r,g,b;
+	double sum_r, sum_g, sum_b;
+	int w,col=0,row=0;
+	double **filter;
+	char buf[BUF_MAX];
+    if(argv[1]==NULL||argv[2]==NULL||!strcmp(argv[1],argv[2])){
+         printf("argument error\n");
+         exit(-1);
+    }
+    //第一引数で指定されたファイルを読み出し用にオープン
+    if((in=fopen(argv[1],"r"))==NULL){
+         printf("file open error for %s\n",argv[1]);
+         exit(-1);
+    }
+    //第二引数で指定されたファイルを書き出し用にオープン
+    if((out=fopen(argv[2],"wb"))==NULL){
+         printf("file open error for %s\n",argv[2]);
+         exit(-1);
+    }
+	if((fin=fopen(argv[3],"r")) == NULL) {
+		printf("file open error for %s\n",argv[3]);
+		exit(-1);
+	}
+	im = gdImageCreateFromJpeg(in);
+
+	width = gdImageSX(im);
+	height = gdImageSY(im);
+
+	// need to read filter here
+	fgets(buf,BUF_MAX,fin); row++;
+	char *ptr = strtok(buf,",");
+	while(ptr != NULL) {
+		col++;
+		ptr = strtok(NULL,",");
+	}
+	while(fgets(buf,BUF_MAX,fin)) row++;
+	if(row != col) {
+		printf("Please Input right filter, row and column has different number\n");
+		exit(-1);
+	}
+
+	filter=(double**)malloc(sizeof(double*)*row);
+	for(i=0;i<row;i++) filter[i] = (double*)malloc(sizeof(double) * col);
+
+	rewind(fin);
+	i=0;
+	while(fgets(buf,BUF_MAX,fin)) {
+		ptr = strtok(buf,",");
+		j = 0;
+		while(ptr != NULL) {
+			sscanf(ptr,"%lf,",&filter[i][j++]);
+			ptr = strtok(NULL,",");
+		}
+		i++;
+	}
+	w = row/2;
+	im_pad = gdImageCreateTrueColor(width+2*w,height+2*w);
+	gdImageCopy(im_pad,im,w,w,0,0,width,height);
+	im_new = gdImageCreateTrueColor(width,height);
+
+	for(j=w;j<height+w;j++) {
+		for(i=w;i<width+w;i++) {
+			sum_r = sum_g = sum_b = 0.0;
+			for(l=0;l<=2*w;l++) {
+				for(k=0;k<=2*w;k++) {
+					pixel = gdImageGetPixel(im_pad,i-w+k,j-w+l);
+					sum_r += (double)gdImageRed(im_pad,pixel) * filter[l][k];
+					sum_g += (double)gdImageGreen(im_pad,pixel) * filter[l][k];
+					sum_b += (double)gdImageBlue(im_pad,pixel) * filter[l][k];
+				}
+			}
+			color = gdImageColorExact(im_new,(int)sum_r,(int)sum_g,(int)sum_b);
+			gdImageSetPixel(im_new,i-w,j-w,color);
+		}
+	}
+	gdImageJpeg(im_new,out,-1);
+
+	fclose(in);
+	fclose(out);
+	fclose(fin);
+	return 0;
+}
---
+++ compscienceAmm_cs_exe/JEONHAESUNG_Week2/Optional1/opt1.c
@@ -0,0 +1,99 @@
+#include<gd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+int bilinear_sum(double x,double y, gdImagePtr im) {
+	double r,g,b;
+	int pixel;
+	int fx,fy;
+	r = g = b = 0.0;
+	fx = (int)floor(x);
+	fy = (int)floor(y);
+
+	pixel = gdImageGetPixel(im,fx,fy);
+	r += (fx+1-x)*(fy+1-y)*gdImageRed(im,pixel);
+	g += (fx+1-x)*(fy+1-y)*gdImageGreen(im,pixel);
+	b += (fx+1-x)*(fy+1-y)*gdImageBlue(im,pixel);
+	
+	pixel = gdImageGetPixel(im,fx,fy+1);
+	r += (fx+1-x)*(y-fy)*gdImageRed(im,pixel);
+	g += (fx+1-x)*(y-fy)*gdImageGreen(im,pixel);
+	b += (fx+1-x)*(y-fy)*gdImageBlue(im,pixel);
+	
+	pixel = gdImageGetPixel(im,fx+1,fy);
+	r += (x-fx)*(fy+1-y)*gdImageRed(im,pixel);
+	g += (x-fx)*(fy+1-y)*gdImageGreen(im,pixel);
+	b += (x-fx)*(fy+1-y)*gdImageBlue(im,pixel);
+	
+	pixel = gdImageGetPixel(im,fx+1,fy+1);
+	r += (fx-x)*(fy-y)*gdImageRed(im,pixel);
+	g += (fx-x)*(fy-y)*gdImageGreen(im,pixel);
+	b += (fx-x)*(fy-y)*gdImageBlue(im,pixel);
+	
+	return 256*256*(int)r + 256*(int)g + (int)b;
+}
+
+int valid(int x, int y, int width, int height) {
+	if( x<0 || x >= width) return 0;
+	else if ( y<0 || y >= height) return 0;
+	return 1;
+}
+
+int main(const int argc, const char *argv[]) {
+    FILE *out, *in;
+    gdImagePtr im,im_new;
+    int width, height, i, j, pixel, color,r,g,b;
+	int new_width, new_height;
+	int x,y,xd,yd;
+	int sum;
+	double sx, sy;
+
+    if(argv[1]==NULL||argv[2]==NULL||!strcmp(argv[1],argv[2])){
+         printf("argument error\n");
+         exit(-1);
+    }
+    //第一引数で指定されたファイルを読み出し用にオープン
+    if((in=fopen(argv[1],"r"))==NULL){
+         printf("file open error for %s\n",argv[1]);
+         exit(-1);
+    }
+    //第二引数で指定されたファイルを書き出し用にオープン
+    if((out=fopen(argv[2],"wb"))==NULL){
+         printf("file open error for %s\n",argv[2]);
+         exit(-1);
+    }
+	if(argc < 4) {
+		printf("missing sx or sy, input correctly\n");
+		exit(-1);
+	}
+	sx = atof(argv[3]);
+	sy = atof(argv[4]);
+	im = gdImageCreateFromJpeg(in);
+
+	width = gdImageSX(im);
+	height = gdImageSY(im);
+	new_width = width * sx;
+	new_height = height * sy;
+	im_new = gdImageCreateTrueColor(new_width,new_height);
+	for(yd=0;yd<new_height;yd++) {
+		for(xd=0;xd<new_width;xd++) {
+			if(!valid((int)((double)xd/sx),(int)((double)yd/sy),width,height)) continue;
+			sum = bilinear_sum((double)xd/sx,(double)yd/sy,im);
+			r = sum / (256*256);
+			g = (sum / 256) % 256;
+			b = sum % 256;
+
+			color = gdImageColorExact(im_new,r,g,b);
+			gdImageSetPixel(im_new,xd,yd,color);
+		}
+	}
+
+	gdImageJpeg(im_new,out,-1);
+
+	fclose(in);
+	fclose(out);
+
+	return 0;
+}
